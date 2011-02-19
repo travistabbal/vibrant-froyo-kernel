69c5677364f1086827134d9838f8c5d3180b0c08
Travis Tabbal <travis@tabbal.net>
2011-02-19 12:57:15 -0700

    Voodoo sound v4

--- Kernel/sound/soc/codecs/wm8994_voodoo.h
+++ Kernel/sound/soc/codecs/wm8994_voodoo.h
@@ -9,5 +9,14 @@
 void voodoo_hook_fmradio_headset(void);
 unsigned int voodoo_hook_wm8994_write(struct snd_soc_codec *codec, unsigned int reg, unsigned int value);
 void voodoo_hook_wm8994_pcm_probe(struct snd_soc_codec *codec);
+void voodoo_hook_record_main_mic(void);
+void voodoo_hook_playback_headset(void);
 void update_hpvol(void);
-void update_fm_radio_headset_restore_bass(bool with_mute);
+void update_fm_radio_headset_restore_freqs(bool with_mute);
+void update_fm_radio_headset_normalize_gain(void);
+void update_recording_preset(void);
+void update_full_bitwidth(bool with_mute);
+void update_osr128(void);
+void update_fll_tuning(void);
+unsigned short tune_fll_value(unsigned short val);
+void update_mono_downmix(void);
